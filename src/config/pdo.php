03b6f4c979a961127e361833c3d1e6b1eae95a92
Gasyh3 <kevrakt@gmail.com>
2026-01-04 00:58:26 +0100

    fix pdo

--- src/config/pdo.php
+++ src/config/pdo.php
@@ -1,14 +1,18 @@
 <?php
-$host = 'db';
-$dbname = 'influmatch';
-$user = 'influmatch_user';
-$pass = 'influmatch_pass';
+$dbHost = 'db';
+$dbName = 'influmatch';
+$dbUser = 'influmatch_user';
+$dbPassword = 'influmatch_pass';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host={$host};dbname={$dbname};charset={$charset}";
+$dsn = "mysql:host={$dbHost};dbname={$dbName};charset={$charset}";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
-$pdo = new PDO($dsn, $user, $pass, $options);
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPassword, $options);
+} catch (PDOException $e) {
+    exit('Erreur de connexion a la base de donnees');
+}
